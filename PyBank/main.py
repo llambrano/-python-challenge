d849e6d83b58e50aa3a1f7a662d7977414346f30
Libardo Lambrano <libardo@syndikomm.com>
2020-06-22 11:02:17 -0400

    total months and net total amount of "Profit/Losses" over the entire period

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,88 +6,37 @@ import csv
 file = os.path.join('resources', 'budget_data.csv')
 
 with open(file) as f:
-    
     reader = csv.reader(f, delimiter=',')
     header_row = next(reader)
-        # print(header_row) if want to review what my headers are
     
     for index, column_header in enumerate(header_row):
-        print(index, column_header)
+        print(index, column_header) # to match index with header
+    
+    #create lists from columns 
+    months = [] # empty list to store months = []
+    profit_loss = [] # empty list to store profit/loss = []
 
-    # empty function total_months = []
-    months = []
-    for row in reader: 
-        months.append(row[0]) # add months column 0 
+    for row in reader:  
+        months.append(row[0]) # add all months column 0 
+        profit_loss.append(int(row[1])) # add all prof/loss column 1
 
-    # empty function profit/loss = []
-    # profit_loss = []
-    # for row in reader:
-    #     profit_loss.
-
-# def pybank(bankdata):
-
-# data reading 
-# date = str(f[0])
-# profit_loss = int(f[1])
+# calculations 
+total_months = len(months)
+total_profit_loss = sum(profit_loss) # https://bit.ly/3fPiiiC
 
 
-# total months 
-# total_months = len(rows)
-
-# profit loss 
-    #total profit loss from column 2 = index(1)
-
-# greatest increase vs previous month
-
-    # - get previous time period 
-    # - calculate p(n) - p(n-1)
-    # loop holding to highest value 
-    # list = numbers 
-    # last_row =  next (csvfile)
-    # max = 0
-    # for x in l:
-    #     if x > max: 
-    #         max = x
-
-    #     last_row = row 
-
-# greatest decrease 
-
-# 
-
-# loading variables as list 
-
-
-# The total number of months included in the dataset
-
-
-# The net total amount of "Profit/Losses" over the entire period
-
-
-# The average of the changes in "Profit/Losses" over the entire period
-
-    # def average(numbers):
-    #     lenght = len(numbers)
-    #     total = 0.0
-    #     for number in numbers:
-
-
-# The greatest increase in profits (date and amount) over the entire period
-
-
-# The greatest decrease in losses (date and amount) over the entire period
-
-# As an example, your analysis should look similar to the one below:
-
-# Financial Analysis
-# ----------------------------
-# Total Months: 86
-# Total: $38382578
-# Average  Change: $-2315.12
-# Greatest Increase in Profits: Feb-2012 ($1926159)
-# Greatest Decrease in Profits: Sep-2013 ($-2196167)
+    # Profit & Loss 
+    # avg_profit_loss = []
+    # pl_1 = []
+        
+        
+        # Avg change 
+        # for i in range(len(profit)-1):
+        #     pl_1.append()
 
 # print results 
+
 print('Financial Analysis')
 print('----------------------------')
-print('Total Months: ' + str(len(months)))
+print(f'Total Months: {(total_months)}') # or print('Total Months: ' + str(len(months))) 
+print(f"Total: ${(total_profit_loss)}")
