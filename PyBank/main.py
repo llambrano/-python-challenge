fa8aaee493e8a3abe8a18e0e520ef07558e5d666
Libardo Lambrano <libardo@syndikomm.com>
2020-06-22 11:28:29 -0400

    average included

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,14 +15,20 @@ with open(file) as f:
     #create lists from columns 
     months = [] # empty list to store months = []
     profit_loss = [] # empty list to store profit/loss = []
+    change = []
+    c = 1 # counter start
 
-    for row in reader:  
+    for row in reader: # loop to append rows to lists
         months.append(row[0]) # add all months column 0 
         profit_loss.append(int(row[1])) # add all prof/loss column 1
+    
+    for c in range(len(profit_loss)-1):
+        change.append(profit_loss[c+1]-profit_loss[c])
 
 # calculations 
 total_months = len(months)
 total_profit_loss = sum(profit_loss) # https://bit.ly/3fPiiiC
+avg = round(sum(change) / (len(profit_loss)-1),2)
 
 
     # Profit & Loss 
@@ -40,3 +46,4 @@ print('Financial Analysis')
 print('----------------------------')
 print(f'Total Months: {(total_months)}') # or print('Total Months: ' + str(len(months))) 
 print(f"Total: ${(total_profit_loss)}")
+print(f"Average  Change: ${(avg)}")
